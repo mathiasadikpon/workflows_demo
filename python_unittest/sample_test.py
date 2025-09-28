7a1f547567c869b5429e14340424589f43ed705a
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-09-28 11:17:38 -0400

    update

--- python_unittest/sample_test.py
+++ python_unittest/sample_test.py
@@ -5,24 +5,29 @@ def sum(x, y):
     return result
 
 class SampleTest(unittest.TestCase):
-    @unittest.expectedFailure
-    def test_sum_pos_int(self):
+    def test_sum(self):
         self.assertEqual(sum(4, 6), 0)
-    
-    def test_sum_neg_int(self):
         self.assertEqual(sum(-4, -6), -10)
-
-    def test_sum_pos_float(self):
         self.assertEqual(sum(4.5, 6.2), 10.7)
-    @unittest.skip('demonstrating skipping')
-    def test_sum_neg_float(self):
         self.assertEqual(sum(-4.5, -6.2), -10.7)
+    # @unittest.expectedFailure
+    # def test_sum_pos_int(self):
+    #     self.assertEqual(sum(4, 6), 0)
+    
+    # def test_sum_neg_int(self):
+    #     self.assertEqual(sum(-4, -6), -10)
 
-    def setUp(self):
-        print("\nRun setUp")
+    # def test_sum_pos_float(self):
+    #     self.assertEqual(sum(4.5, 6.2), 10.7)
+    # @unittest.skip('demonstrating skipping')
+    # def test_sum_neg_float(self):
+    #     self.assertEqual(sum(-4.5, -6.2), -10.7)
 
-    def tearDown(self):
-        print("Run tearDown")
+    # def setUp(self):
+    #     print("\nRun setUp")
+
+    # def tearDown(self):
+    #     print("Run tearDown")
 
 if __name__ == '__main__':
     unittest.main()
