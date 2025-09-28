baa2b2990f77ad93c74fa801c2ecd45df8199297
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-09-28 10:20:32 -0400

    update

---
+++ basic_pytest/test_classes.py
@@ -0,0 +1,19 @@
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+
+# def test_classes_compare():
+#     p1 = Person("Mindy")
+#     p2 = Person("Daniel")
+#     assert p1.name == p2.name
+
+import pytest
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+def test_classes_compare():
+    p1 = Person("Mindy")
+    p2 = Person("Mindy")
+    assert p1 == p2
