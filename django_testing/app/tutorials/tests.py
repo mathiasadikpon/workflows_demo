172b84a6e8f4c5582e0895cc1c5678430e97c184
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-09-28 12:30:16 -0400

    update

--- django_testing/app/tutorials/tests.py
+++ django_testing/app/tutorials/tests.py
@@ -9,12 +9,22 @@ def test_homepage_access():
           url = reverse('home')
           assert url == "/"
 
-@pytest.mark.django_db
-def test_create_tutorial():
+# @pytest.mark.django_db
+# def test_create_tutorial():
+#     tutorial = Tutorial.objects.create(
+#         title='Pytest',
+#         tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+#         description='Tutorial on how to apply pytest to a Django application',
+#         published=True
+#     )
+#     assert tutorial.title == "Pytest"
+
+@pytest.fixture
+def new_tutorial(db):
     tutorial = Tutorial.objects.create(
         title='Pytest',
         tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
         description='Tutorial on how to apply pytest to a Django application',
         published=True
     )
-    assert tutorial.title == "Pytest"
+    return tutorial
